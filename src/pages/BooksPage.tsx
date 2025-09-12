9c383485506fdc4d95e84a8dd15e0d55a3d493cc
Julian Nordin <julian.nordin94@gmail.com>
2025-09-12 18:27:33 +0200

    feat: render book list on BooksPage

--- src/pages/BooksPage.tsx
+++ src/pages/BooksPage.tsx
@@ -1,7 +1,21 @@
+import { useBooks } from '../features/books/hooks/useBooks'
+import BookCard from '../features/books/components/BookCard'
+
 function BooksPage() {
+  const { data: books, isLoading, isError } = useBooks()
+
   return (
     <div>
       <h1>Books</h1>
+      {isLoading && <p>Loading books…</p>}
+      {isError && <p>Something went wrong loading books.</p>}
+      {books && (
+        <div>
+          {books.map((book) => (
+            <BookCard key={book.id} book={book} />
+          ))}
+        </div>
+      )}
     </div>
   )
 }
