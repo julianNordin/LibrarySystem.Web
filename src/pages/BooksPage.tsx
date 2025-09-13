7187c17db0ee033a3ffa6e520beb45c58f07be65
Julian Nordin <julian.nordin94@gmail.com>
2025-09-13 10:44:05 +0200

    refactor: use shared UI state components in book list

--- src/pages/BooksPage.tsx
+++ src/pages/BooksPage.tsx
@@ -1,5 +1,8 @@
 import { useBooks } from '../features/books/hooks/useBooks'
 import BookCard from '../features/books/components/BookCard'
+import LoadingSpinner from '../components/LoadingSpinner'
+import ErrorMessage from '../components/ErrorMessage'
+import EmptyState from '../components/EmptyState'
 
 function BooksPage() {
   const { data: books, isLoading, isError } = useBooks()
@@ -7,9 +10,10 @@ function BooksPage() {
   return (
     <div>
       <h1>Books</h1>
-      {isLoading && <p>Loading books…</p>}
-      {isError && <p>Something went wrong loading books.</p>}
-      {books && (
+      {isLoading && <LoadingSpinner label="Loading books…" />}
+      {isError && <ErrorMessage message="Something went wrong loading books." />}
+      {books && books.length === 0 && <EmptyState message="No books available yet." />}
+      {books && books.length > 0 && (
         <div>
           {books.map((book) => (
             <BookCard key={book.id} book={book} />
